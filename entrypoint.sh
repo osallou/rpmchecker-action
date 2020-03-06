14c77c7e328475cffdc97050be736e3ebbcf7186
Olivier Sallou <olivier.sallou@irisa.fr>
2020-03-06 13:27:32 +0100

    set HOME

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,11 @@ tar cvfz /home/builder/rpm/${name}-${version}.tar.gz -C /tmp ${name}-${version}
 rm -rf /tmp/${name}-${version}
 
 echo "build package"
-mkdir -p /tmp/build
-rpmbuild -ba --buildroot /tmp/build /home/builder/rpm/*.spec
+export HOME=/home/builder
+rpmbuild -ba /home/builder/rpm/*.spec
 
 echo "lint packages"
-rpmlint /tmp/build/*/*.rpm
+rpmlint /home/builder/rpm/*/*.rpm
 
 echo "rpm version: ${version}-${release}"
 echo ::set-output name=rpmversion::${version}-${release}
